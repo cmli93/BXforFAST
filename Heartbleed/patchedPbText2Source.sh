d5082b2fdafda61f02c40a608bf96b499295f72e
Yijun Yu <yijun.yu@gmail.com>
2017-09-21 14:10:56 +0100

    update

--- Heartbleed/patchedPbText2Source.sh
+++ Heartbleed/patchedPbText2Source.sh
@@ -1,6 +1,6 @@
-javac ReverseProcessProto.java
+#javac ReverseProcessProto.java
 #我是注释行
-java ReverseProcessProto modifiedPbProcessed.txt modifiedPb.txt
+#java ReverseProcessProto modifiedPbProcessed.txt modifiedPb.txt
 ./ComPBBiYacc get modifiedPb.txt srcAST
 ./ComXMLBiYacc put srcProcessed.xml srcAST
 javac ReverseProcessXML.java
