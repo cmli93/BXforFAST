bbe09aeebf8e82ce29a66a644d7cc743a3ee0916
cmli93 <chunmiaoli1993@gmail.com>
2017-10-01 11:01:51 +0800

    just produce srcAST

--- Heartbleed/patchedPbText2Source.sh
+++ Heartbleed/patchedPbText2Source.sh
@@ -1,6 +1,6 @@
-#javac ReverseProcessProto.java
+javac ReverseProcessProto.java
 #我是注释行
-#java ReverseProcessProto
+java ReverseProcessProto
 ./ComPBBiYacc get modifiedPb.txt srcAST
 ./ComXMLBiYacc put srcProcessed.xml srcAST
 javac ReverseProcessXML.java
--- Heartbleed/source2PbText.sh
+++ Heartbleed/source2PbText.sh
@@ -3,13 +3,3 @@ javac ProcessXML.java
 #我是注释行
 java ProcessXML
 ./ComXMLBiYacc get srcProcessed.xml srcAST
-if [ ! -d "/srcPb.txt" ]; then
-  sudo mkdir /srcPb.txt
-fi
-echo "" > srcPb.txt
-./ComPBBiYacc put srcPb.txt srcAST
-javac ProcessProto.java
-#我是注释行
-java ProcessProto
-cat srcPbProcessed.txt | protoc --encode=fast.Data fast.proto>src.pb
-cat src.pb | protoc --decode=fast.Data fast.proto>srcPbProcessed.txt
